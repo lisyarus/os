7413e02690f32a7a4fe02f048e5e659b28571f6b
lisyarus <lisyarus@gmail.com>
2013-05-21 23:27:54 +0400

    More cosmetic changes to dupme

--- dupme/main.c
+++ dupme/main.c
@@ -3,35 +3,60 @@
 #include <stdlib.h>
 #include <string.h>
 
-void _full_write (int fd, char * buffer, int len)
+void full_write (int fd, const char * buffer, int len)
 {
     while (len != 0)
     {
         int k = write(fd, buffer, len);
+        if (k == 0) exit(1);
         len -= k;
         buffer += k;
     }
 }
 
+int safe_read (int fd, void * buf, int count)
+{
+    const char * error_msg = "Error while reading\n";
+    int result = read(fd, buf, count);
+    if (result == -1)
+    {
+        full_write(1, error_msg, strlen(error_msg));
+        exit(1);
+    }
+    return result;
+}
+
+char * safe_malloc (int size)
+{
+    const char * error_msg = "Error while allocation memory\n";
+    char * result = (char *)malloc(size);
+    if (result == NULL)
+    {
+        full_write(1, error_msg, strlen(error_msg));
+        exit(1);
+    }
+    return result;
+}
+
 int main (int argc, char ** argv)
 {
-    char * usage_str = "Usage: ./main <buffer_size>\n";
+    const char * usage_str = "Usage: ./main <buffer_size>\n";
     if (argc != 2)
     {
-        _full_write(1, usage_str, strlen(usage_str));
+        full_write(1, usage_str, strlen(usage_str));
         return -1;
     }
     
-    char * wrong_argument = "Second argument should be a positive number\n";
+    const char * wrong_argument = "Second argument should be a positive number\n";
     int buffer_size = atoi(argv[1]);
     if (buffer_size <= 0)
     {
-        _full_write(1, wrong_argument, strlen(wrong_argument));
+        full_write(1, wrong_argument, strlen(wrong_argument));
         return -1;
     }
     
     ++buffer_size;
-    char * buffer = malloc(buffer_size);
+    char * buffer = safe_malloc(buffer_size);
     int length = 0;
     int begin = 0;
     int end = 0;
@@ -53,11 +78,11 @@ int main (int argc, char ** argv)
         int block;
         if (state == normal)
             if (end >= begin)
-                block = read(0, buffer + end, buffer_size - end);
+                block = safe_read(0, buffer + end, buffer_size - end);
             else
-                block = read(0, buffer + end, buffer_size - length);
+                block = safe_read(0, buffer + end, buffer_size - length);
         else
-            block = read(0, buffer, buffer_size);
+            block = safe_read(0, buffer, buffer_size);
         if (block == 0)
         {
             if (length == buffer_size || state == 1)
@@ -66,11 +91,6 @@ int main (int argc, char ** argv)
             buffer[end % buffer_size] = '\n';
             block = 1;
             quit = 1;
-        }
-        if (block == -1)
-        {
-            char * read_error = "An error occured while reading\n";
-            _full_write(1, read_error, strlen(read_error));
         }
 
         int i;
@@ -85,11 +105,11 @@ int main (int argc, char ** argv)
                     for (; count < 2; ++count)
                     {
                         if (begin <= end + i)
-                            _full_write(1, buffer + begin, length + i + 1);
+                            full_write(1, buffer + begin, length + i + 1);
                         else
                         {
-                            _full_write(1, buffer + begin, buffer_size - begin);
-                            _full_write(1, buffer, end + i + 1);
+                            full_write(1, buffer + begin, buffer_size - begin);
+                            full_write(1, buffer, end + i + 1);
                         }
                     }
                     length = - i - 1;
