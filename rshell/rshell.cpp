c6c96ee97572d39ad6ad087295c582e358c93e78
lisyarus <lisyarus@gmail.com>
2013-05-24 16:40:16 +0400

    Makefile and rshell.cpp base

---
+++ rshell/rshell.cpp
@@ -0,0 +1,16 @@
+// C library
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+
+// C++ library
+
+#include <iostream>
+
+// main
+
+int main ( )
+{
+    std::cout << "Hello, world!" << std::endl;
+}
